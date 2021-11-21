280a5aca73321caf5b1ef1dc3dffdb2bfa66c277
p0lycarpio <arsene.reymond@free.fr>
2021-11-21 14:59:37 +0100

    functionnal script

--- oldway.js
+++ oldway.js
@@ -1,3 +1,5 @@
+// Make the sum of digits in a string
+
 for (let i = 0; i < str.length; i++) {
     if (!isNaN(parseInt(str[i]))) {
         sum += parseInt(str[i])
--- script.js
+++ script.js
@@ -1,39 +1,40 @@
 const alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-let input = 'E123456789';
-let bigin= "E99"
+
+// test values
+let input = 'E123456789'
+let bigin = "E99999999999"
+let intex = 875
+
+// Make the sum of each digit present in a string or in an integer
 
 function sumDigits(str) {
     let sum = 0
 
- 
-    if (!isNaN(str)) {
+    if (!isNaN(str)) {      // integer
         while (str) {
             sum += str % 10;
             str = Math.floor(str / 10);
         }
-    }
-    else {
+    } else {                // string
         sum = str.match(/\d/g).reduce((n, c) => n + +c, 0);
     }
-
     return sum
 }
 
 
-
+// Run the function and iterates to have number < 15
 
 let FirstSum = sumDigits(input)
-console.log('FirstSum :', FirstSum)
+console.log('FirstSum:', FirstSum)
+let SumLoop = FirstSum
 
-/* while (FirstSum > 15) {
-    Reduced = sumDigits(FirstSum)
-    return Reduced
-} */
-
-if (FirstSum > 15) {
-    let NumericKey = sumDigits(FirstSum);
-    console.log('NumericKey', NumericKey)
-    console.log(alphabet[NumericKey])
+while (SumLoop > 15) {
+    SumLoop = sumDigits(SumLoop)
+    console.log('Looping: ', SumLoop)
 }
 
-else {console.log('error')}
+// Find alphabetic key
+
+let NumericKey = SumLoop;
+let key = alphabet[NumericKey]
+console.log('NumericKey: ', NumericKey, ' => Correspond to: ', key)
