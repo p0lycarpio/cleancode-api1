6e0f331579b523dc6554204b9f4788f139525508
p0lycarpio <arsene.reymond@free.fr>
2021-12-05 12:05:21 +0100

    functionnal API

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from "express"
+import programme from './script.js'
+const app = express()
+const router = express.Router();
+const port = 8080
+
+app.post("/foo", function (req, res) {
+    res.send('foo');
+});
+
+
+app.get('/:id/', (req, res) => {
+    const id = req.params.id
+    res.send(id)
+})
+
+app.get('/client/:id/verification', (req, res) => {
+    const id = req.params.id
+    let response = programme(id)
+
+    res.status(200).send({
+        status: "OK",
+        request: id,
+        result: response
+    })
+})
+
+
+//////////////
+var route, routes = [];
+app.use("/", router);
+
+app._router.stack.forEach(function (middleware) {
+    if (middleware.route) { // routes registered directly on the app
+        let path = middleware.route.path
+        let method = Object.keys(middleware.route.methods)[0].toUpperCase()
+        routes.push({
+            "path": path,
+            "method": method
+        });
+        return app._router.stack
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send({
+        routes
+    })
+})
+/////////////
+
+app.listen(port, () => {
+    console.log(`Serveur à l\'écoute sur http://localhost:${port}`)
+})
--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 const alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
-// test values
-let input = 'E123456789'
-let bigin = "E99999999999"
-let intex = 875
-
 // Make the sum of each digit present in a string or in an integer
 
 function sumDigits(str) {
     let sum = 0
 
-    if (!isNaN(str)) {      // integer
+    if (!isNaN(str)) { // integer
         while (str) {
             sum += str % 10;
             str = Math.floor(str / 10);
         }
-    } else {                // string
+    } else { // string
         sum = str.match(/\d/g).reduce((n, c) => n + +c, 0);
     }
     return sum
 }
 
+// Compare abc key
 
-// Run the function and iterates to have number < 15
-
-let FirstSum = sumDigits(input)
-console.log('FirstSum:', FirstSum)
-let SumLoop = FirstSum
-
-while (SumLoop > 15) {
-    SumLoop = sumDigits(SumLoop)
-    console.log('Looping: ', SumLoop)
+function compare(input, k) {
+    let i = input.substr(0, 1)
+    return (i == k) ? 1 : 0;
 }
 
-// Find alphabetic key
 
-let NumericKey = SumLoop;
-let key = alphabet[NumericKey]
-console.log('NumericKey: ', NumericKey, ' => Correspond to: ', key)
+export default function programme(input) {
+    // Run the function and iterates to have number < 15
+    let FirstSum = sumDigits(input)
+    let SumLoop = FirstSum
+
+    while (SumLoop > 15) {
+        SumLoop = sumDigits(SumLoop)
+    }
+
+    // Find alphabetic key
+    let key = alphabet[SumLoop]
+    return compare(input, key)
+}
